4cb8ab671885da22d3ddbb3303b940efbfb693ae
cvensand <cvensand@umich.edu>
2019-03-08 19:55:51 +0100

    Up to submitting form data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './Table.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <body>
+                <div className="App">
+                    <h1>Hello, React!</h1>
+                </div>
+                <div className="container">
+                    <Table
+                        characterData={this.state.characters}
+                        removeCharacter={this.removeCharacter}
+                    />
+                </div>
+            </body>
+        );
+    }
+
+    state = {
+        characters: [
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            }
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
 }
 
 export default App;
